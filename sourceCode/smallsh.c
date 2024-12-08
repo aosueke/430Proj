f64c63f87defc82e1374c226150d03ccc7276151
aosueke <91437037+aosueke@users.noreply.github.com>
2024-12-07 20:39:45 -0500

    Update smallsh.c

--- sourceCode/smallsh.c
+++ sourceCode/smallsh.c
@@ -1,25 +1,9 @@
-/* smallsh.h -- defs for smallsh command processor */
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/wait.h>
-
-
-#define EOL        1   /* end of line */
-#define ARG        2   /* normal arguments */
-#define AMPERSAND  3
-#define SEMICOLON  4
-
-#define MAXARG     512   /* max. no. command args */
-#define MAXBUF     512   /* max. langth input line */
-
-#define FOREGROUND 0
-#define BACKGROUND 1
-
-/* smallsh.c */
+/* include file for example */
 #include "smallsh.h"
-/* program buffers and work pointers */
-static char inpbuf[MAXBUF], TOKBUF[2*MAXBUF], 
-*ptr = inpbuf, *tok = TOKBUF;
+
+static char inpbuf[MAXBUF], tokbuf[2*MAXBUF],
+*ptr = inpbuf, *tok = tokbuf;
+
 /* print prompt and read a line */
 int  userin(char *p)
 {
@@ -52,5 +36,123 @@ tok = tokbuf;
         count = 0;
         printf("%s ", p);
       }
+    }
+}
+
+
+/* get token, place into tokbuf */
+int gettok(char **outptr)
+{
+  int type;
+/* set the output string to tok */
+  *outptr = tok;
+/* strip white space from the buffer containing  the tokens */
+  while( *ptr == ' ' || *ptr == '\t')
+    ptr++;
+/* set the token pointer to the first token in the buffer */
+  *tok++ = *ptr;
+/* set the type variable depending
+ * on the token in the buffer */
+switch(*ptr++){
+  case '\n':
+           type = EOL;
+           break;
+  case '&':
+           type = AMPERSAND;
+           break;
+  case ';':
+           type = SEMICOLON;
+           break;
+  default:
+           type = ARG;
+           /* keep reading valid ordinary characters */
+           while(inarg(*ptr))
+                 *tok++ = *ptr++;
+}
+
+*tok++ = '\0';
+  return type;
+}
+
+static char special [] = { ' ', '\t', '&', ';', '\n', '\0'};
+
+int inarg(char c)
+{
+  char *wrk;
+  for(wrk = special; *wrk; wrk++)
+    if(c == *wrk)
+      return (0);
+}
+return (1);
+}
+
+int procline(void)     /* process input line */
+{
+  char *arg[MAXARG+1]; /* pointer array for runcommand */
+  int toktype;         /* type of token in command */
+  int narg;            /* number of arguments so far */
+  int type;            /* FOREGROUND or BACKGROUND */
+
+  narg = 0;
+
+  for(;;)              /* loop forever */
+  {
+      /* take action according to token type */
+      switch(toktype = gettok(&arg[narg])){
+        case ARG: if(narg < MAXARG)
+                  narg++;
+              break;
+        case EOL:
+        case SEMICOLON:
+        case AMPERSAND:
+              if(toktype == AMPERSAND)
+                  type = BACKGROUND;
+              else
+                  type = FOREGROUND;
+              if(narg != 0)
+              {
+                  arg[narg] = NULL;
+                  runcommand(arg, type);
+              }
+              if(toktype == EOL)
+                  return;
+        narg = 0;
+        break;
+      }
+/* execute a command with optional wait */
+int runcommand(char **cline, int where)
+    {
+      pid_t pid;
+      int status;
+
+      switch(pid = fork()){
+        case -1:
+              perror("smallsh");
+              return (-1);
+        case 0:
+              execvp(*cline, cline);
+              perror(*cline);
+              exit(1);
+    }
+/* code for parent */
+/* if background process print pid and exit */
+
+if(where == BACKGROUND)
+{
+      printf("[Process id %d]\n", pid);
+      return (0);
+}
+/* wait until process pid exits */
+if(waitpid(pid, &status, 0) == -1)
+      return (-1);
+else
+      return (status);
+}
+    char *prompt = "Command> "; /* prompt */
+
+    main()
+    {
+      while(userin(prompt) != EOF)
+        procline();
     }
 }
