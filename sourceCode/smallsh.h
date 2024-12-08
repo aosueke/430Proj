7002f67a56f911bc7166374589418391bf7b8be2
aosueke <91437037+aosueke@users.noreply.github.com>
2024-12-07 21:16:25 -0500

    Update smallsh.h

--- sourceCode/smallsh.h
+++ sourceCode/smallsh.h
@@ -1,21 +1,15 @@
-#include <signal.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "joblist.h"
 
-#define MAX_JOBS 10  /* Maximum number of jobs */
+#define EOL 1
+#define ARG 2
+#define AMPERSAND 3
+#define SEMICOLON 4
 
-/* Job states */
-#define RUNNING 0
-#define STOPPED 1
-#define TERMINATED 2
+#define MAXARG 512
+#define MAXBUF 512
 
-/* Structure for jobs */
-typedef struct {
-    int job_id;
-    pid_t pgid;   // Process group ID
-    int state;    // RUNNING, STOPPED, or TERMINATED
-    char command[MAXBUF]; // Command line
-} job_t;
-
-job_t jobs[MAX_JOBS];  // Array of jobs
-int job_count;         // Number of jobs
+#define FOREGROUND 1
+#define BACKGROUND 0
